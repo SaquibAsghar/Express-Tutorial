2dd92335e06c5334f13a6fa10cbc1f7a8be33e9c
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-12 21:34:32 +0530

    Update app.js
    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Express Tutorial");
+const http  = require("http")
+
+const PORT = process.env.PORT || 5000
+
+const server = http.createServer((req, res)=>{
+    res.end("Welcome")
+})
+
+
+server.listen(PORT, ()=>{
+    console.log(`Server is up and runing at port ${PORT}`);
+})
