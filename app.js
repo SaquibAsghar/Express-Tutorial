e57796674ac9f77abc79ff8d8f2b865395febb2e
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-12 21:37:26 +0530

    Update app.js
    added response header

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const http  = require("http")
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req, res)=>{
-    res.end("Welcome")
+    res.writeHead(200,{'content-type':'text/html'})
+    res.write('<h1>Welcome</h1>')
+    res.end()
 })
 
 
