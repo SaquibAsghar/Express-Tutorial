75dc72964eadf213864d77d169af291be508cf16
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-12 23:08:05 +0530

    Update app.js
    Added URL endpoint and switch case

--- app.js
+++ app.js
@@ -5,9 +5,25 @@ const PORT = process.env.PORT || 5000
 const server = http.createServer((req, res)=>{
     console.log(`REQUEST METHOD : ${req.method}`);
     console.log(`REQUEST URL : ${req.url}`);
-    res.writeHead(200,{'content-type':'text/html'})
-    res.write('<h1>Welcome</h1>')
-    res.end()
+    let requestURL = req.url
+    switch (requestURL) {
+        case '/':
+            res.writeHead(200, {'content-type': 'text/html'})
+            res.write('<h1>Welcome</h1>')
+            res.end()
+            break;
+        case '/about':
+            res.writeHead(200, {'content-type': 'text/html'})
+            res.write('<h1>This is About page</h1>')
+            res.end()
+            break
+        default:
+            res.writeHead(404, {'content-type': 'text/html'})
+            res.write('<h1>Page Not Found</h1>')
+            res.end()
+            break;
+    }
+    
 })
 
 
