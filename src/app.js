2cc2b21cf5aa135954820e0f69765fac237bfeca
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-18 00:56:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const logger = require("./loggerMiddleware");
+const authorize = require('./authorizeMiddlware')
 
 const app = express();
 
 // Using Middleware
-app.use('/api',logger)
+app.use([logger, authorize])
 // this middleware only work in path containg api/anypath
 
-app.get("/", logger, (req, res) => {
-	res.status(200).send("Home page");
+app.get("/", (req, res) => {
+	res.status(200).send("Home page").end();
 });
-app.get("/about", logger, (req, res) => {
-	res.status(200).send("About page");
+app.get("/about", (req, res) => {
+	res.status(200).send("About page").end();
 });
-app.get("/api/products", logger, (req, res) => {
-	res.status(200).send("Product page");
+app.get("/api/products", (req, res) => {
+	res.status(200).send("Product page").end();
 });
-app.get("/api/items", logger, (req, res) => {
-	res.status(200).send("Items page");
+app.get("/api/items", (req, res) => {
+	res.status(200).send("Items page").end();
 });
 
 app.listen(3001, () => console.log("Server is up and running"));
