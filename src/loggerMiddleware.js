b2e3474c13799d7b068d5e5b4fa5fef43b0747ad
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-18 00:56:46 +0530

    Update loggerMiddleware.js

--- src/loggerMiddleware.js
+++ src/loggerMiddleware.js
@@ -2,7 +2,7 @@ const logger = (req, res, next)=>{
     const url = req.url
        const time = new Date().getFullYear()
        const method = req.method
-       console.log(url, method, time);
+       console.log(`URL = ${url}\n Method = ${method}\n Time = ${time}`);
        next()
    }
 
