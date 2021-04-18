d7830ee2c2444d6eecc92abc92cd2e291586cc12
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-18 16:43:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
 const path = require('path')
@@ -5,8 +6,29 @@ let { people } = require("./data");
 
 app.use(express.static(path.join(__dirname, '../public')))
 
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.json())
+
 app.get("/api/people", (req, res) => {
 	res.status(200).json({ success: true, data: people });
 });
 
+app.post("/api/people", (req, res) => {
+    const {name} = req.body
+    if(name){
+        return res.status(201).json({ success: true, person: name }).end();
+    }
+    return res.status(401).json({status: false, msg:'Please provide name value'}).end()
+	
+});
+
+// POST request
+app.post('/login', (req, res)=>{
+    const {name} = req.body
+    if(name){
+        return res.status(200).send(`<h1>Welcome, ${name}</h1>`).end()
+    }
+    return res.status(401).send('Please provide credential').end()
+})
 app.listen(3001, () => console.log("Server is up and running"));
