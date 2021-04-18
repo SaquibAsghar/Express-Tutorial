597a50b0125264ac2ddf2dd6415c9952e2edd795
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-18 11:27:53 +0530

    Update app.js
    Added path

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 let { people } = require("./data");
 
-app.use(express.static('../public'))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get("/api/people", (req, res) => {
 	res.status(200).json({ success: true, data: people });
