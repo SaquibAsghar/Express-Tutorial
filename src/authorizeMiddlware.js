f391002728add12fb9dad89ce65600eeda66ed06
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-18 09:49:42 +0530

    Update authorizeMiddlware.js

--- src/authorizeMiddlware.js
+++ src/authorizeMiddlware.js
@@ -1,6 +1,15 @@
 const authorize = (req, res, next)=>{
-    console.log('Authorize')
-    next()
+    const {userName} = req.query
+    if (userName){
+        req.user={
+            name: "John",
+            id: 201
+        }
+        next()
+    }
+    else{
+        return res.status(401).send('<h2>Unauthorize</h2>').end()
+    }
 }
 
 module.exports = authorize
