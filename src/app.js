2a3671f3604de2ea46b91be0b616eccdee942453
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-18 11:15:05 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 const express = require("express");
-const app = express()
+const app = express();
+let { people } = require("./data");
+
+app.get("/api/people", (req, res) => {
+	res.status(200).json({ success: true, data: people });
+});
+
 app.listen(3001, () => console.log("Server is up and running"));
