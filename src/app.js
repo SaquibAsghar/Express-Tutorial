002732f33f298c3d534c21333cf503ba00871428
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-18 10:47:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,3 @@
 const express = require("express");
-const logger = require("../middleware/loggerMiddleware");
-const authorize = require('../middleware/authorizeMiddlware')
-const morgan = require('morgan')
-
-const app = express();
-
-// Using Middleware
-// app.use([logger, authorize])
-// this middleware only work in path containg api/anypath
-
-app.use(morgan('tiny'))
-
-app.get("/", (req, res) => {
-	res.status(200).send("Home page").end();
-});
-app.get("/about", (req, res) => {
-	res.status(200).send("About page").end();
-});
-app.get("/api/products", (req, res) => {
-	res.status(200).send("Product page").end();
-});
-app.get("/api/items", (req, res) => {
-	res.status(200).send("Items page").end();
-});
-
+const app = express()
 app.listen(3001, () => console.log("Server is up and running"));
