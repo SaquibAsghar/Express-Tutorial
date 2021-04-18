99b14056cfe8a79201881860aa09e008400767a6
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-04-18 17:02:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path')
 
 const peopleRoute = require('../router/routes/people')
+const peopleLogin = require('../router/routes/auth')
 
 app.use(express.static(path.join(__dirname, '../public')))
 
@@ -12,13 +13,9 @@ app.use(express.json())
 
 app.use('/api/people', peopleRoute)
 
+app.use('/login', peopleLogin)
+
 
 // POST request
-app.post('/login', (req, res)=>{
-    const {name} = req.body
-    if(name){
-        return res.status(200).send(`<h1>Welcome, ${name}</h1>`).end()
-    }
-    return res.status(401).send('Please provide credential').end()
-})
+
 app.listen(3001, () => console.log("Server is up and running"));
